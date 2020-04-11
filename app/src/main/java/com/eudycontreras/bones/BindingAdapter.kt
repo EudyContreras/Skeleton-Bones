9c92466a02da288a7e6fb8c77310244d37bcc1c9
Eudy Contreras <Darkyec777@gmail.com>
2020-04-11 03:02:48 +0200

    small update

--- app/src/main/java/com/eudycontreras/bones/BindingAdapter.kt
+++ app/src/main/java/com/eudycontreras/bones/BindingAdapter.kt
@@ -1,10 +1,16 @@
 package com.eudycontreras.bones
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.eudycontreras.boneslibrary.extensions.notifySkeletonImageLoaded
 
 /**
  * Copyright (C) 2020 Project X
@@ -20,6 +26,29 @@ fun ImageView.loadImage(imageUrl: String?) {
 
     Glide.with(context)
         .load(imageUrl)
+        .addListener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                exception: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                this@loadImage.notifySkeletonImageLoaded()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                this@loadImage.notifySkeletonImageLoaded()
+                return false
+            }
+
+        })
         .apply(RequestOptions().format(DecodeFormat.PREFER_ARGB_8888))
         .into(this)
 }
